919ee6188621a3971a314bf09863c7ba0603f0e2
Nano Montoya Oliveros <nanohack@gmail.com>
2017-03-30 17:07:16 +0200

    add shuffle feature and enable play file

--- js/app.js
+++ js/app.js
@@ -1,12 +1,56 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    initPlayer();
     getSongs();
 });
-var audio;
+var audio = document.getElementById('player');
 var music;
 
-function getSongs(){
-    $.getJSON("js/app.json", function(mjson){
-        music = mjson;
-        console.log(music);
-    })
+function initPlayer() {
+    $('#shuffle').click(function () {
+        $("#playlist").empty();
+        music.songs =  shuffle(music.songs);
+        genList(music);
+        playSong(0);
+    });
+}
+
+function getSongs() {
+    $.getJSON("/js/app.json", function (result) {
+        music = result;
+        genList(music);
+    });
+}
+
+function genList(music) {
+    $.each(music.songs, function (i, song) {
+        $("#playlist").append('<li class="list-group-item" id="' + i + '">' + song.name + '</li>');
+    });
+    $('#playlist li').click(function () {
+        var selectedSong = $(this).attr('id');
+        playSong(selectedSong);
+    });
+}
+
+function playSong(selectedSong) {
+    var long = music.songs;
+    if (selectedSong >= long.length) {
+        audio.pause();
+    } else {
+        $('#player').attr('src', music.songs[selectedSong].song);
+        audio.play();
+        scheduleSong(selectedSong);
+    }
+
+}
+
+function scheduleSong(id) {
+    audio.onended = function () {
+        playSong(parseInt(id) + 1);
+
+    }
+}
+
+function shuffle(array) {    
+    for (var random, temp, position = array.length; position; random = Math.floor(Math.random() * position), temp = array[--position], array[position] = array[random], array[random] = temp);
+    return array;
 }
