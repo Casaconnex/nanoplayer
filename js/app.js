3d5f904c80b2787db4abfa950756850e2ed375ea
Nano Montoya Oliveros <nanohack@gmail.com>
2017-04-04 15:24:43 +0200

    materialize css applied add save playlist feature bug fixes

--- js/app.js
+++ js/app.js
@@ -1,18 +1,49 @@
 //var audio = document.getElementById('player');
 var music;
+var songs = [];
 
 $(document).ready(function () {
+    $('.tooltipped').tooltip({ delay: 50 });
     initPlayer();
 });
 
 
 function initPlayer() {
-    $('#shuffle').click(function () {
+    $('#shuffle').click(function () {        
         $("#playlist").empty();
         music = shuffle(music);
         genList(music);
         playSong(0);
     });
+
+    $('#playlist_clean').click(function () {
+        $("#playlist").empty();
+        player.pause();        
+        player.src = '';
+        music = null;
+        songs = null;
+    });
+
+    $('#playlist_download').click(function () {
+        if (songs.length > 0) {
+            var jsonToSave = JSON.stringify(songs);
+            var file = new Blob([jsonToSave], { type: 'text/plain' });
+            var fileUrl = URL.createObjectURL(file);
+            var downloadLink = document.createElement("a");
+            downloadLink.download = "playlist.npl";
+            downloadLink.innerHTML = "Download File";
+            downloadLink.href = fileUrl;
+            downloadLink.onclick = destroyClickedElement;
+            downloadLink.style.display = "none";
+            document.body.appendChild(downloadLink);
+
+            downloadLink.click();
+        }
+    });
+}
+
+function destroyClickedElement(event) {
+    document.body.removeChild(event.target);
 }
 
 function getSongs() {
@@ -24,9 +55,9 @@ function getSongs() {
 
 function genList(music) {
     $.each(music, function (i, song) {
-        $("#playlist").append('<li class="list-group-item" id="' + i + '">' + song.name + '</li>');
+        $("#playlist").append('<a class="collection-item">' + song.name.substring(song.name.lastIndexOf('.'), 0) + '<i><img id="' + i + '" src="img/play.png" align="right"></img></i></a>');
     });
-    $('#playlist li').click(function () {
+    $('#playlist img').click(function () {
         var selectedSong = $(this).attr('id');
         playSong(selectedSong);
     });
@@ -39,7 +70,7 @@ function playSong(selectedSong) {
     } else {
         $('#player').attr('src', music[selectedSong].song);
         player.play();
-        notifyUser(music[selectedSong].name);
+        notifyUser(music[selectedSong].name.substring(music[selectedSong].name.lastIndexOf('.'), 0));
         scheduleSong(selectedSong);
     }
 
@@ -68,6 +99,7 @@ function scheduleSong(id) {
 }
 
 function shuffle(array) {
-    for (var random, temp, position = array.length; position; random = Math.floor(Math.random() * position), temp = array[--position], array[position] = array[random], array[random] = temp);
+    if (array.length > 0)
+        for (var random, temp, position = array.length; position; random = Math.floor(Math.random() * position), temp = array[--position], array[position] = array[random], array[random] = temp);
     return array;
 }
