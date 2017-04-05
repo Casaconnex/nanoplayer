bceddaf132a9f3acba044f38663b54b187b370d7
Nano Montoya Oliveros <nanohack@gmail.com>
2017-04-05 18:01:12 +0200

    add get music from cloud feature

--- js/app.js
+++ js/app.js
@@ -5,17 +5,75 @@ var songs = [];
 $(document).ready(function () {
     $('.tooltipped').tooltip({ delay: 50 });
     $(".button-collapse").sideNav({
-      menuWidth: 300, // Default is 300
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true // Choose whether you can drag to open on touch screens
+        menuWidth: 300, // Default is 300
+        edge: 'right', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
     });
     initPlayer();
+
+
 });
 
+function handleFileFromCloud(evt) {
+    var url = $("#music_cloud_url").val();
+    $.ajax({        
+        url: url,
+        success: function (data) {
+            $(data).find("a:contains(.mp3)").each(function () {
+                if (songs === null)
+                    songs = [];
+
+                songs.push({
+                    id: $(this).position(),
+                    name: decodeURI($(this).attr("href")),
+                    song: decodeURI(url + $(this).attr("href"))
+                });                
+            });
+
+            music = songs;
+            $("#playlist").empty();
+            genList(music);
+        },
+        error: function (request, error) {
+            console.log(error);
+        }
+    });
+}
+
+function handleFileUpload(evt) {
+    var fileToLoad = evt.target.files[0];
+    var fileReader = new FileReader();
+    fileReader.onload = function (fileLoadedEvent) {
+        var textFromFileLoaded = fileLoadedEvent.target.result;
+        music = JSON.parse(textFromFileLoaded);
+        $("#playlist").empty();
+        genList(music);
+    };
+    fileReader.readAsText(fileToLoad, "UTF-8");
+}
+
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+
+    if (songs === null)
+        songs = [];
+    // files is a FileList of File objects. List some properties.           
+    for (var i = 0, f; f = files[i]; i++) {
+        if (f.type === "audio/mp3")
+            songs.push({
+                id: i,
+                name: f.name,
+                song: URL.createObjectURL(f)
+            });
+    }
+    music = songs;
+    $("#playlist").empty();
+    genList(music);
+}
 
 function initPlayer() {
-    $('#shuffle').click(function () {        
+    $('#shuffle').click(function () {
         $("#playlist").empty();
         music = shuffle(music);
         genList(music);
@@ -24,7 +82,7 @@ function initPlayer() {
 
     $('#playlist_clean').click(function () {
         $("#playlist").empty();
-        player.pause();        
+        player.pause();
         player.src = '';
         music = null;
         songs = null;
@@ -46,24 +104,17 @@ function initPlayer() {
             downloadLink.click();
         }
     });
-
-    $('#pl_upload').click(function () {
-       //todo
-    });
-
 }
 
 function destroyClickedElement(event) {
     document.body.removeChild(event.target);
 }
 
-function loadPlayList()
-{
+function loadPlayList() {
     var fileToLoad = document.getElementById("pl_upload").files[0];
- 
+
     var fileReader = new FileReader();
-    fileReader.onload = function(fileLoadedEvent) 
-    {
+    fileReader.onload = function (fileLoadedEvent) {
         var textFromFileLoaded = fileLoadedEvent.target.result;
         console.log(textFromFileLoaded);
     };
