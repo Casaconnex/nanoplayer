c7ae40016f6dcc0893b87dd1e5c4aa59a197b89d
nanohack <nanohack@gmail.com>
2017-04-13 22:12:59 +0200

    add override of audio features

---
+++ js/audio.js
@@ -0,0 +1,17 @@
+function CreateSeekBar() {
+  var seekbar = document.getElementById("audioSeekBar");
+  seekbar.min = 0;
+  seekbar.max = player.duration;
+  seekbar.value = 0;
+}
+
+function audioSeekBar() {
+  var seekbar = document.getElementById("audioSeekBar");
+  player.currentTime = seekbar.value;
+}
+
+function SeekBar() {
+  var seekbar = document.getElementById("audioSeekBar");
+  seekbar.value = player.currentTime;
+}
+
